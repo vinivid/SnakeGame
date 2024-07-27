587d7a6623ca1fadbcae9e7a4b1bc2320980b733
vini <frato.vini@gmail.com>
2024-07-27 15:27:50 -0300

    added colision on the snake and the end of the map

--- headers/snake.hpp
+++ headers/snake.hpp
@@ -26,18 +26,22 @@ struct body_part{
 class Snake{
     private:
         std::set<int> positions;
+        //Todo try to not use list
         std::list<std::pair<int, direction>> index_turn;
         std::vector<body_part> v{3};
+        //Todo change variable names to be more apropiate
+        //Todo make it so the movement of the snake is not dependent on framerate
         int routine_time = 6;
         int count_cicle = 6;
         bool used_queue = false;
         float routine_change = 0.017f;
-        int prev_removed = 183;
-        int prev_added = 185;
+        int prev_removed = 185;
+        int prev_added = 183;
 
         void add_part();
-        int check_next_pos(int p_added, int row_next, int colum_next, fruit& frt);
-        void init_trhee();
+        int check_next_pos(int row_next, int colum_next, fruit& frt);
+        void init_snake();
+        void manage_prev_added();
     public:
         Snake();
 
--- libraries/Snake/snake.cxx
+++ libraries/Snake/snake.cxx
@@ -17,7 +17,7 @@
 #include <thread>
 
 Snake::Snake(){
-    init_trhee();
+    init_snake();
 }
 
 int Snake::move(fruit& frt){
@@ -55,61 +55,58 @@ int Snake::move(fruit& frt){
             }
         }
         
-        int row_prev = prev_added/20;
-        int colum_prev = prev_added%20;
+        int row_prev = prev_removed/20;
+        int colum_prev = prev_removed%20;
+        int res = 0;
 
-        positions.insert(prev_removed);
         switch(v[0].dir){
             case up:
                 row_prev -= 1;
-                prev_added -= 20;
-                if(check_next_pos(prev_added-20, row_prev-1, colum_prev,frt))
+                prev_removed -= 20;
+                res = check_next_pos(row_prev, colum_prev,frt);
+                if(res == 0){
+                    manage_prev_added();
+                }else if(res == 1){
                     return 1;
+                }
 
-                positions.erase(prev_added);
+                positions.erase(prev_removed);
                 break;
             case down:
                 row_prev += 1;
-                prev_added += 20;
-                if(check_next_pos(prev_added+20, row_prev+1,colum_prev,frt))
+                prev_removed += 20;
+                res = check_next_pos( row_prev,colum_prev,frt);
+                if(res == 0){
+                    manage_prev_added();
+                }else if(res == 1){
                     return 1;
+                }
 
-                positions.erase(prev_added);
+                positions.erase(prev_removed);
                 break;
             case left:
                 colum_prev -= 1;
-                prev_added -= 1;
-                if(check_next_pos(prev_added-1, row_prev, colum_prev-1,frt))
+                prev_removed -= 1;
+                res = check_next_pos(row_prev, colum_prev,frt);
+                if(res == 0){
+                    manage_prev_added();
+                }else if(res == 1){
                     return 1;
+                }
 
-                positions.erase(prev_added);
+                positions.erase(prev_removed);
                 break;
             case right:
                 colum_prev += 1;
-                prev_added += 1;
-                if(check_next_pos(prev_added+1,row_prev, colum_prev+1, frt))
-                    return 1;
-
-                positions.erase(prev_added);
-                break;
-            default:
-                break;
-        }
-
-        //std::cout << "prev added: " << prev_added << "\n";
-
-        switch(v[v.size()-1].dir){
-            case up:
-                prev_removed -= 20;
-                break;
-            case down:
-                prev_removed += 20;
-                break;
-            case left:
-                prev_removed -= 1;
-                break;
-            case right:
                 prev_removed += 1;
+                res = check_next_pos(row_prev, colum_prev, frt);
+                if(res == 0){
+                    manage_prev_added();
+                }else if(res == 1){
+                    return 1;
+                }
+
+                positions.erase(prev_removed);
                 break;
             default:
                 break;
@@ -122,8 +119,6 @@ int Snake::move(fruit& frt){
     return 0;
 }
 
-//Checks if an arrow key has been pressed, if it has been, put on the list, for it to
-//change the direction of the snake
 void Snake::key_press(GLFWwindow *window){
     if(v[0].dir != down && glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
         if(!used_queue){
@@ -173,20 +168,22 @@ void Snake::draw_snake(Control &ctrl, Shader &shd){
 
 void Snake::new_snake(){
     v.resize(3);
-    init_trhee();
+    init_snake();
     index_turn.resize(0);
     prev_removed = 183;
-    prev_added = 185;
+    prev_removed = 185;
     used_queue = false;
     count_cicle = 6;
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(3s);
 }
 
+//Todo fix adding or removing wrong position whe the snake is on the edge
 void Snake::add_part(){
     body_part to_add;
     to_add.x = 0; to_add.y = 0; to_add.scale = 1; to_add.dir = right;
 
+    positions.erase(prev_added);
     switch(v[v.size() - 1].dir){
         case up:
             to_add.x = v[v.size() - 1].x;
@@ -219,20 +216,20 @@ void Snake::add_part(){
     v.push_back(to_add);
 }
 
-int Snake::check_next_pos(int p_added, int row_next, int colum_next, fruit& frt){
-    if(prev_added == frt.pos_coord){
+int Snake::check_next_pos(int row_next, int colum_next, fruit& frt){
+    if(prev_removed == frt.pos_coord){
         add_part();
         frt.gen_new_fruit(positions);
-        return 0;
+        return 2;
     }
     
-    if(row_next >= 21 || row_next <= -2 || colum_next >= 21 || colum_next <= -2){
+    if(row_next <= -1 || row_next >= 20 || colum_next >= 20 || colum_next <= -1 || positions.find(prev_removed) == positions.end()){
         return 1;
     }
     return 0;
 }
 
-void Snake::init_trhee(){
+void Snake::init_snake(){
     for(int i = 0; i < 400; i++) positions.insert(i);
     positions.erase(185);
     positions.erase(184);
@@ -275,4 +272,24 @@ void Snake::init_trhee(){
         v[i].dir = v[i-1].dir;
         v[i].scale = v[i-1].scale - 0.0024f;
     }
+}
+
+void Snake::manage_prev_added(){
+    positions.insert(prev_added);
+    switch(v[v.size()-1].dir){
+            case up:
+                prev_added -= 20;
+                break;
+            case down:
+                prev_added += 20;
+                break;
+            case left:
+                prev_added -= 1;
+                break;
+            case right:
+                prev_added += 1;
+                break;
+            default:
+                break;
+        }
 }
